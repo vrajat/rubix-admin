1c30e00df803cb9908fec55028ac0f748a4596e4
Vamshi P <vamship@qubole.com>
2017-08-16 12:39:45 +0530

    create req directories, copy rubix jars and change presto filesystem impl

--- rubix_admin/installer.py
+++ rubix_admin/installer.py
@@ -1,6 +1,7 @@
 import logging
 
 from fabric.operations import sudo, put, os
+from fabric.contrib.files import append
 from fabric.state import env
 from fabric.tasks import execute
 from fabric.utils import abort
@@ -18,13 +19,12 @@ class Installer:
                                                        help="Install from RPM")
         cls.install_parser.add_argument("-r", "--rpm", required=True,
                                         help="Path to RPM file")
-        cls.install_parser.add_argument("-a", "--rpm-args", default="",
+        cls.install_parser.add_argument("-a", "--rpm-args", default="--ignoreos",
                                         help="Arguments to rpm command")
         cls.install_parser.set_defaults(func=cls.install_cmd)
 
     @classmethod
     def install_cmd(cls, args):
-        logging.info("Installing %s" % args.rpm)
         return execute(cls.install, args, hosts=args.config["hosts"])
 
     @classmethod
@@ -50,7 +50,16 @@ class Installer:
 
     @classmethod
     def _rpm_install(cls, args):
-        return sudo('rpm -U %s %s' %
+            logging.warn("in rpm install")
+        rpm_output = sudo('rpm -U %s %s' %
                     (args.rpm_args,
                      os.path.join(args.config["remote_packages_path"],
                                   os.path.basename(args.rpm))))
+
+        sudo("cp -a /usr/lib/rubix/lib/* /usr/lib/presto/lib/plugin/hive-hadoop2/")
+        sudo("cp -a /usr/lib/rubix/lib/* /usr/lib/hadoop/lib/")
+        sudo("mkdir -p /media/ephemeral0")
+
+        append("/usr/lib/presto/etc/catalog/hive.properties","hive.fs.s3n.impl=com.qubole.rubix.presto.CachingPrestoS3FileSystem", True)
+        append("/usr/lib/presto/etc/catalog/hive.properties","hive.fs.s3.impl=com.qubole.rubix.presto.CachingPrestoS3FileSystem", True)
+        append("/usr/lib/presto/etc/catalog/hive.properties","hive.fs.s3a.impl=com.qubole.rubix.presto.CachingPrestoS3FileSystem", True)
